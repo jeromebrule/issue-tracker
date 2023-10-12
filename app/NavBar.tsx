7ea5ff1a7edb03407b41088ad145a185abcf8f76
jeromeVeritree <jerome.brule@veritree.com>
2023-10-12 09:18:58 -0700

    navbar - styles

---
+++ app/NavBar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import React from "react";
+import {AiFillBug} from "react-icons/ai";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link, index) => (
+          <li
+            key={index}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
