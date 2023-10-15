04877429ad7706716da21f17b9507b6b469c884e
jeromeVeritree <jerome.brule@veritree.com>
2023-10-15 13:29:32 -0700

    edit page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
 import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import EditIssueButton from "../EditIssueButton";
+import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 
 interface Props {
