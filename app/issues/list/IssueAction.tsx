a4d2e96c10de0758cbad2a1229bc45adc3304631
jeromebrule <jerome.brule@gmail.com>
2023-10-17 09:03:53 -0700

    filter issues

--- app/issues/list/IssueAction.tsx
+++ app/issues/list/IssueAction.tsx
@@ -6,10 +6,10 @@ import IssueStatusFilter from "./IssueStatusFilter";
 const IssueAction = () => {
   return (
     <Flex mb="5" justify="between">
+      <IssueStatusFilter />
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
-      <IssueStatusFilter />
     </Flex>
   );
 };
