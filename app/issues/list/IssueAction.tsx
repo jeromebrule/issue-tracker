69bfa62789ab4a837f9d6f94cc66db7d120edf44
jeromebrule <jerome.brule@gmail.com>
2023-10-17 08:55:13 -0700

    status filter

--- app/issues/list/IssueAction.tsx
+++ app/issues/list/IssueAction.tsx
@@ -1,14 +1,16 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import React from "react";
 import Link from "next/link";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueAction = () => {
   return (
-    <div className="mb-5">
+    <Flex mb="5" justify="between">
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
-    </div>
+      <IssueStatusFilter />
+    </Flex>
   );
 };
 
