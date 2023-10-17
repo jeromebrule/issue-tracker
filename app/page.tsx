da377d81ef5ce47db0406cfc2e5a2ac82005c745
jeromebrule <jerome.brule@gmail.com>
2023-10-17 11:30:09 -0700

    latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
+import LatestIssues from "./LatestIssues";
 import NavBar from "./NavBar";
 import Pagination from "./components/Pagination";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return (
-    <div>
-      Hello World!{" "}
-      <Pagination
-        currentPage={parseInt(searchParams.page)}
-        itemCount={100}
-        pageSize={10}
-      />
-    </div>
-  );
+export default function Home() {
+  return <LatestIssues />;
 }
